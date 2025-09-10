59ce48d647cb59da72174be4963225e18f4b732e
Sameera Jape <sameerajape@gmail.com>
2025-09-10 11:37:31 +0530

    path & query params basic implementation

--- 1-HelloWorld-Basic Api/main.py
+++ 1-HelloWorld-Basic Api/main.py
@@ -5,6 +5,14 @@ app= FastAPI()
 def home_route():
     return {"message_key1":"Welcome to home page"}
 
-@app.get("/home2")
+@app.get("/home2") #static route
 def home_route():
     return {"Welcome to home page2"}
+
+@app.get("/cats/{breed}") #path params used in route
+def cats(breed :str):
+    return {f"This cat is of breed {breed}"}
+
+@app.get("/kittens/{breed_type}") #query params also added
+def cats(breed_type :str, color :str="orange", age: int|None = None):
+    return {f"This cat is of breed {breed_type} and color {color}. \n It's age is {age}."}
