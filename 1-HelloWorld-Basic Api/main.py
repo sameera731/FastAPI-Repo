3a7431fc0bbfef06dace9afcac8420b023b18d72
Sameera Jape <sameerajape@gmail.com>
2025-09-10 12:19:04 +0530

    Regex validation from pydantic

--- 1-HelloWorld-Basic Api/main.py
+++ 1-HelloWorld-Basic Api/main.py
@@ -1,18 +1,54 @@
+
+#  1-HelloWorld-Basic Api
 from fastapi import FastAPI
+from pydantic import constr
 app= FastAPI()
 
+
+#EP1
 @app.get("/")
 def home_route():
     return {"message_key1":"Welcome to home page"}
 
+#EP2
 @app.get("/home2") #static route
 def home_route():
     return {"Welcome to home page2"}
 
+#EP3
 @app.get("/cats/{breed}") #path params used in route
 def cats(breed :str):
     return {f"This cat is of breed {breed}"}
 
-@app.get("/kittens/{breed_type}") #query params also added
+#EP4
+@app.get("/kittens/{breed_type}") #now query params added
 def cats(breed_type :str, color :str="orange", age: int|None = None):
     return {f"This cat is of breed {breed_type} and color {color}. \n It's age is {age}."}
+
+'''
+In ep4 above, if in url user passed color=7 still it would not raise error as json validation would convert
+int 7 to string 7 and 7 would still be accepted as str only. But if say we had constraint for int and user 
+passes abc then error would be raised cause while the prev conversion is possible 7-> "7", "abc" to int 
+is not possible.
+
+But if you want to handle 7->"7" more strictly and not allow it then you can do 3 ways:
+1. Use enum [most professional]: make an enum class of all possible values and use it.
+2. use Regex constraint  in the line:- color: constr(regex="^[A-Za-z]+$")
+
+in new pydantic version regex keyword is updated by "pattern" - SUPPPPPER IMP
+
+means that only letters are allowed in color.
+3. Manually put condition inside function before returning result: eg: if condition etc...
+
+eg:shown in ep 5,6,7
+'''
+
+#Regex handling for author name
+@app.get("/route5/{book_name}/{author}")
+def fn5 (book_name: str, author: constr(pattern="^[A-Za-z]+$"), page: int=1):
+    return{"Book Name: ": book_name,
+           "Author: ": author,
+           "page: ": page
+           }
+
+
