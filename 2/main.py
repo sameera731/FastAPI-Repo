d3403f3239a589c488f287700607fd072a4a876b
Sameera Jape <sameerajape@gmail.com>
2025-09-12 12:44:09 +0530

    update feat, getoneuser exception handl not working

--- 2/main.py
+++ 2/main.py
@@ -12,7 +12,7 @@ class UserDesign(BaseModel):
 class UserCreate(UserDesign):
     pass #users objects will be made of this when user gives info to db. same design as blueprint design
 
-class User(BaseModel):
+class User(UserDesign):
     id:int #id will be added by DB 
 
 #fake db
@@ -26,7 +26,7 @@ def get_next_id():
     next_id+=1
     return uid
 
-#Create user root by client side
+#Create user route by client side
 @app.post("/users/", response_model=User)
 def makeuser(user: UserCreate):
     user_id=get_next_id()
@@ -34,8 +34,38 @@ def makeuser(user: UserCreate):
     users_db[user_id]=new_user
     return new_user
 
+@app.get("/")
+def homefn():
+    return "Welcome to server home page"
+
+@app.get("/users/{userid}", response_model=User) #to get one user by their id
+def getOneUser(userid:int ):
+    user=users_db[userid]
+    if not user:
+        raise HTTPException(404, "user not found")
+    return user
 
 
+@app.get("/users/", response_model=List[User]) #to get all users listed. list of type user
+def getAllUsers():
+    return list(users_db.values())
+
+#for udpdate- 'put'
+@app.put("/users/{userid}", response_model=User)
+def updatefn(userid : int, user: User):
+    if userid not in users_db:
+        raise HTTPException(404, "User not found")
+    updated_user=User(id=userid, **user.model_dump())
+    users_db[userid]=updated_user
+    return updated_user
+
+#for delete- 'delete'
+@app.delete("/users/{userid}")
+def deletefn(userid: int):
+    if userid not in users_db:
+        raise HTTPException(404, "User not found")
+    del users_db[userid]
+    return {"message: ": "user deleted"}
 
 
 
