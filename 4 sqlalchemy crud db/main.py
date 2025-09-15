4043a1ab9bb49f01d7746af8157755f8b2ce689a
Sameera Jape <sameerajape@gmail.com>
2025-09-15 16:10:53 +0530

    get and post i.e enter and read routes done

--- 4 sqlalchemy crud db/main.py
+++ 4 sqlalchemy crud db/main.py
@@ -1,5 +1,5 @@
-from typing import Dict, Optional
-from fastapi import FastAPI, HTTPException,status
+from typing import Dict, List, Optional
+from fastapi import FastAPI, HTTPException, Depends, status
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Integer, String, Boolean, Column
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
@@ -50,7 +50,39 @@ class TodoResponse(BaseModel):
     class Config:
         orm_mode = True              #SUPER IMP
 
+#___________________________________________schema work done_________________________________
+
 # All setup and design done, now fastapi app and routes
+
+app=FastAPI()
+
+@app.post("/todos/", response_model=TodoResponse, status_code=status.HTTP_201_CREATED)
+def create_todo(todo: TodoCreate, db: Session= Depends(get_db)):  #Depends is keyword
+    db_todo=Todo(**todo.model_dump())  #in db we store an entry of type orm model of table so in it we unwind the received pydantic model argument.
+    try:
+        db.add(db_todo)
+        db.commit()
+        db.refresh(db_todo)
+    except Exception:
+        db.rollback() #to keep the transaction safe and cancel all incase error
+        raise
+    return db_todo
+
+@app.get("/todos/", response_model=List[TodoResponse])            #? what if i write list instead of List
+def read_todos(skip:int =0, limit: int=100, db: Session=Depends(get_db)):
+    return db.query(Todo).offset(skip).limit(limit).all()          #?  what if jsut db.query(Todo).all()
+
+@app.get("/todos/{todo_id}", response_model=TodoResponse)
+def read_todo(todo_id:int , db: Session=Depends(get_db)):
+    fetched_todo= db.query(Todo).filter(Todo.id==todo_id).first()
+    if not fetched_todo:
+        raise HTTPException(404, "Not found")
+    return fetched_todo
+
+
+
+
+
 
 
 
